ec00707f3881f16d336a2f9d227e235135e6d908
Tsutomu Kawamura <kawamura@cognitom.com>
2014-08-18 16:50:15 +0900

    gulp.watch の追加

--- recipe/02-hello-gulp/gulpfile.js
+++ recipe/02-hello-gulp/gulpfile.js
@@ -4,11 +4,15 @@ var autoprefixer = require('gulp-autoprefixer');
 var minifyCss    = require('gulp-minify-css');
 var rename       = require('gulp-rename');
 
-gulp.task('style', function(){             // define a task `coffee`
+gulp.task('style', function(){             // define a task `style`
   return gulp.src('src/style.css')         // read files from `src`
     .pipe(cssimport())                     // 1. process @import directive
     .pipe(autoprefixer('last 2 versions')) // 2. add/remove vender prefixes
     .pipe(minifyCss())                     // 3. minify css file
     .pipe(rename({ extname: '.min.css' })) // 4. change the file extention
     .pipe(gulp.dest('dist/'));             // save it to `dist`
+});
+
+gulp.task('watch', function() {            // define a task `watch`
+  gulp.watch(['src/*.css'], ['style']);    // run `style` task if files update
 });
